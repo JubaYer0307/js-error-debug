123545bdde90bb70277e13780d8379c067c956b6
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-04-04 16:14:44 +0600

    debugging

---
+++ reference-error.js
@@ -0,0 +1,21 @@
+const x = 'pori'
+const y = 'moni'
+// console.log(z);
+
+// console.log(name);
+// const name = 'Ananta jalil';
+// let name = 'Ananta jalil';
+// var name = 'Ananta jalil';
+
+// console.log(add2(2,3));
+function add(n1, n2){
+
+    return n1+n2
+}
+
+const add2 = (n1, n2) => n1 + n2;
+
+function doSomthing (){
+    const sum = 44;
+}
+console.log(sum);
---
+++ type-error.js
@@ -0,0 +1,13 @@
+const numbs = 5;
+
+// numbs.push(45);
+
+// let address = ;
+// console.log(address.home);
+
+let address = { city: 'bBaria' , postCode: 4560} ;
+console.log(address?.home?.building);
+
+const a = 'abul';
+
+a ='babul';
